c640755f12990b9bb4af89132a4475b393ac00ef
Yevve <70255780+Yevve@users.noreply.github.com>
2020-10-09 15:44:16 +0200

    website

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
+app.engine(".hbs",expressHandlebars({
+    defaultLayout:"main.hbs"
+}))
+
+app.use(express.static("static"))
+app.get("/layout.css")
+
+app.get("/",function(request,response){
+response.render("start.hbs")
+})
+
+app.get("/about",function(request,response){
+    response.render("about.hbs")
+})
+
+app.listen(8080)
+
