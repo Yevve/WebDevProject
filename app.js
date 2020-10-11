9e41251157314454d9abe49597a19435a96f19e8
Yevve <70255780+Yevve@users.noreply.github.com>
2020-10-11 19:23:00 +0200

    added create blogs

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser=require('body-parser')
 const app = express()
+
+const blogs=[{
+    id:1,
+    title:"First Post",
+    blogpost: "Here is the first blogpost I made"
+
+}]
+
 app.engine(".hbs",expressHandlebars({
     defaultLayout:"main.hbs"
 }))
 
 app.use(express.static("static"))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.get("/layout.css")
 
 app.get("/",function(request,response){
@@ -19,11 +31,43 @@ app.get("/about",function(request,response){
 app.get("/contact",function(request,response){
     response.render("contact.hbs")
 })
+app.get("/blogs",function(request,response){
+    
+    const model={
+        blogs 
+    }
 
-app.get("/blog",function(request,response){
-    response.render("blog.hbs")
+    response.render("blogs.hbs",model)
+
 })
 
+app.get("/blogs/:id",function(request,response){
+    const id = request.params.id
+
+    const blog = blogs.find(
+        b => b.id == id
+    )
+    const model={
+        blog
+    }
+
+    response.render("blog.hbs",model)
+
+})
+app.get("/create-blogpost",function(request,response){
+    response.render("create-blogpost.hbs")
+})
+app.post("/create-blogpost",function(request,response){
+    const title = request.body.title
+    const blogpost = request.body.blogpost
+    const blog={
+        title,
+        blogpost,
+        id:blogs.length +1
+    }
+    blogs.push(blog)
+    response.redirect("/blogs/"+blog.id)
+})
 
 app.listen(8080)
 
